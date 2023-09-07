133b6cc9686297766681e39eae5af1032ca6b53e
MustafizAnsari <mustafiz.ansari@gmail.com>
2023-09-08 02:10:28 +0530

    lightning component that can query a list of 10 most recently created accounts

---
+++ force-app/main/default/aura/recentAccontsList/recentAccontsListController.js
@@ -0,0 +1,16 @@
+({
+    doInit : function(component, event, helper) 
+    {
+      var action = component.get("c.getRecentAccounts");
+      action.setCallback(this, function(response){
+          var state = response.getState();
+          if(state === "SUCCESS"){
+            component.set("v.recentAccounts", response.getReturnValue());
+          }
+          else {
+            console.log("Error while fatching recent account"+ state);
+          }
+      });
+      $A.enqueueAction(action);
+    }
+})
---
+++ force-app/main/default/aura/recentAccountsApp/recentAccountsApp.app
@@ -0,0 +1,3 @@
+<aura:application extends="force:slds">
+        <c:recentAccontsList />
+</aura:application>	
